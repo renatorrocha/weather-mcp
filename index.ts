f8ceea41be66580acc5611e83aca87d07191a48b
renatorrocha <renatorrodrigues2002@gmail.com>
2025-12-09 11:28:24 -0300

    feat: setup sdtio transport and dev script

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from "zod";
 
 const server = new McpServer({
@@ -45,6 +46,51 @@ server.registerTool(
 	},
 );
 
+server.registerResource(
+	"flights",
+	"flights://airports",
+	{
+		mimeType: "text/plain",
+		description: "List of supported airports for flights",
+	},
+	async () => {
+		return {
+			contents: [
+				{
+					uri: "flights://airports",
+					mimeType: "text/plain",
+					text: `Supported Airports:
+                - JFK (New York)
+                - LHR (London Heathrow)
+                - SFO (San Francisco)`,
+				},
+			],
+		};
+	},
+);
+
+server.registerResource(
+	"Weather",
+	"weather://cities",
+	{
+		mimeType: "text/plain",
+		description: "List of supported cities for weather data",
+	},
+	async () => {
+		return {
+			contents: [
+				{
+					uri: "weather://cities",
+					mimeType: "text/plain",
+					text: `Supported Cities:
+                    - New York (USA)
+                    - London (UK)`,
+				},
+			],
+		};
+	},
+);
+
 async function getWeatherByCity(city: string) {
 	if (city.toLowerCase() === "new york") {
 		return { temp: "22°C", forecast: "Partly cloudy with a breeze" };
@@ -54,3 +100,17 @@ async function getWeatherByCity(city: string) {
 	}
 	return { temp: null, error: "Weather data not available for this city" };
 }
+
+async function init() {
+	const transport = new StdioServerTransport();
+
+	await server.connect(transport);
+
+	console.log("🌤️  Weather MCP Server Started!");
+	console.log("🛠️  Tool: getWeatherDataByCityName");
+	console.log("📚 Resource: weather://cities");
+	console.log("🏙️  Supported Cities: New York, London");
+	console.log("✅ Server ready!");
+}
+
+init().catch(console.error);
